9c54e8b9d55876dcf9f200a44bc97981c7df6994
Naveen Chaudhary <naveen.c@techaspect.com>
2018-10-06 11:37:12 +0530

    Added Migratory Birds JS Solution

---
+++ Day_10/migratorybirds.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+    var count = [0,0,0,0,0];
+    for(var i=0; i<arr.length; i++) {
+        count[arr[i]-1]++;
+    }
+    var max = count[0], index=0;
+    for(var i=1; i<5; i++){
+        if(count[i]>max){
+            max = count[i];
+            index = i;
+        }
+    }
+    return index+1;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
