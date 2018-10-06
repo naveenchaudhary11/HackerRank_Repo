782c470227d0418df890e3b1961e1c00d863e228
Naveen Chaudhary <naveen.c@techaspect.com>
2018-10-06 13:08:07 +0530

    Added Bon Appetit JS Solution

---
+++ Day_10/bonappetit.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+    var sum=0, res="Bon Appetit";
+    for(var i=0; i<bill.length; i++) {
+        sum+=bill[i];
+    }
+    sum-=bill[k];
+    if(sum != 2*b) {
+        res = b-sum/2;
+    }
+    console.log(res);
+    
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
